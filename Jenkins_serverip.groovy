2f3721e668612a1bde679a2b81a92b00c47a3d39
saraswathirg <4saraswathi95@gmail.com>
2021-11-25 17:25:41 +0530

    added changes

--- Jenkins_serverip.groovy
+++ Jenkins_serverip.groovy
@@ -44,8 +44,8 @@ pipeline{
 //def ipValues = ${SERVER_IP}.split(",")                    
  //scp -o StrictKeyHostchecking=no -i /tmp/awsaws.pem hello-${BUILD_NUMBER}.war ec2-user@$ipValue:/var/lib/tomcat/webapps/
                 script{ 
-                    def ipList = $SERVER_IP               
-                    for (a in ipList)
+                    //def ipList = $SERVER_IP               
+                    for (a in [1])
                     {                  
                         sh """
                             echo $a
